7eeb7ea5b0d233ddb4eb186fee68524e246595b8
amidamarulas <amidamarulasgmail.com>
2025-11-03 15:50:49 -0500

    Mejora a la usabilidad de admin.py

--- core_lab/admin.py
+++ core_lab/admin.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
 from .models import Material, Ensayo, Simulacion
 
+# Panel de administración de materiales
 @admin.register(Material)
 class MaterialAdmin(admin.ModelAdmin):
-    list_display = ['nombre', 'descripcion']
+    list_display = ('nombre', 'tipo', 'modulo_elasticidad', 'limite_elastico')
+    search_fields = ('nombre', 'tipo')
 
+# Panel de administración de ensayos
 @admin.register(Ensayo)
 class EnsayoAdmin(admin.ModelAdmin):
-    list_display = ['id', 'tipo', 'descripcion']
+    list_display = ('id', 'tipo', 'descripcion')
+    search_fields = ('tipo', )
 
+# Panel de administración de simulaciones
 @admin.register(Simulacion)
 class SimulacionAdmin(admin.ModelAdmin):
-    list_display = ['id', 'material', 'ensayo', 'fecha_creacion']
-    readonly_fields = ['fecha_creacion']
+    list_display = ('id', 'material', 'ensayo', 'fecha_creacion')
+    readonly_fields = ('fecha_creacion',)
+    list_filter = ('material', 'ensayo')
